5c6cd14de1562f1c241b365daeebf52b7d0b140c
Jay Sullivan <jay@petio.org>
2011-01-13 11:18:47 -0500

    copyfile: close fds on error

--- copyfile.c
+++ copyfile.c
@@ -3,30 +3,18 @@
 %use stralloc_copys;
 %use stralloc_cats;
 %use stralloc_append;
+%use buffer_init;
+%use buffer_write;
+%use buffer_put;
 */
 #include "error.h"
 #include "stralloc.h"
+#include "buffer.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-static int allwrite(int fd,const char *buf,unsigned int len)
-{
-  int w;
-
-  while (len) {
-    w = write(fd,buf,len);
-    if (w == -1) {
-      if (errno == error_intr) continue;
-      return -1; /* note that some data may have been written */
-    }
-    if (w == 0) ; /* luser's fault */
-    buf += w;
-    len -= w;
-  }
-  return 0;
-}
 static int oneread(int fd,char *buf,unsigned int len)
 {
   int r;
@@ -37,6 +25,8 @@ static int oneread(int fd,char *buf,unsigned int len)
     return r;
   }
 }
+static buffer bout;
+static char bout_buf[8192];
 static int copyfile_tmp(const char *from, const char *to)
 {
   char buf[8192];
@@ -46,17 +36,19 @@ static int copyfile_tmp(const char *from, const char *to)
   int wfd;
   struct stat filestat;
   if((rfd = open(from,O_RDONLY))<0) return -1;
-  if((wfd = open(to,O_WRONLY|O_EXCL|O_CREAT,0644))<0) return -1;
-  if(fstat(wfd,&filestat)<0) return -1;
+  if((wfd = open(to,O_WRONLY|O_EXCL|O_CREAT,0644))<0) { close(rfd); return -1; }
+  if(fstat(wfd,&filestat)<0) { close(rfd); close(wfd); return -1; }
   if(filestat.st_blksize < sizeof(buf)) blocksize = filestat.st_blksize;
   else blocksize = sizeof(buf);
+  buffer_init(&bout,buffer_unixwrite,wfd,bout_buf,blocksize);
   for (;;) {
-      if((n=oneread(rfd,buf,blocksize))<0) return -1;
+      if((n=oneread(rfd,buf,blocksize))<0) { close(rfd); close(wfd); return -1; }
       if(n==0) break;
-      if(allwrite(wfd,buf,n)<0) return -1;
+      if(buffer_putalign(&bout,buf,n)<0) { close(rfd); close(wfd); return -1; }
   }
-  if(close(rfd)<0) return -1;
-  if(fsync(wfd)<0) return -1;
+  if(close(rfd)<0) { close(wfd); return -1; }
+  if(buffer_flush(&bout)<0) { close(wfd); return -1; }
+  if(fsync(wfd)<0) { close(wfd); return -1; }
   if(close(wfd)<0) return -1;
   return 0;
 }
